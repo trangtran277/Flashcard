bbea2744348759dd5bbe02f9a922ad7f9ed92a34
trangtran277 <trangtran.1997@gmail.com>
2020-03-13 22:59:10 -0500

    Set up database to store new flashcards.

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -7,13 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashCards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashCards = flashcardDatabase.getAllCards();
+
+        if(allFlashCards != null && allFlashCards.size() > 0)
+        {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashCards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashCards.get(0).getAnswer());
+        }
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,16 +56,33 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCardDisplayedIndex++;
+
+                if(currentCardDisplayedIndex > allFlashCards.size() - 1)
+                {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashCards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashCards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (requestCode == 100)
+        if (data != null && requestCode == 100)
         {
             String ques = data.getExtras().getString("questString");
             String ans = data.getExtras().getString("answerString");
             ((TextView)findViewById(R.id.flashcard_question)).setText(ques);
             ((TextView) findViewById(R.id.flashcard_answer)).setText(ans);
+            flashcardDatabase.insertCard(new Flashcard(ques, ans));
+            allFlashCards = flashcardDatabase.getAllCards();
         }
     }
 }
